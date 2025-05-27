134b5535d9a9cbb970699e46cdfa03d3dfc9bac7
Hyoukinha <isabellycarolinazyc@gmail.com>
2025-05-27 19:33:55 -0300

    segundo commit sistema.py
    carregando váriaveis e conectando ao banco de dados

--- sistema_gerenciamento.py
+++ sistema_gerenciamento.py
@@ -2,3 +2,16 @@ import mysql.connector
 import pandas as pd
 from dotenv import load_dotenv
 import os
+
+# Carregar variáveis de ambiente
+load_dotenv()
+
+# Conectar ao banco
+def conectar_banco():
+    conexao = mysql.connector.connect(
+        host=os.getenv('DB_HOST'),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD'),
+        database=os.getenv('DB_DATABASE')
+    )
+    return conexao
